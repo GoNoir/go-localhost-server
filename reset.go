6e1dc93d7fe8f6c677a93b1db7340e8b22ad2fb7
GoNoir <weisbrodt.stefan+1@gmail.com>
2025-11-10 20:10:07 +0000

    Metrics and reset redirected to different endpoint and metrics now renders from html rather than converting string into slice of bytes

--- reset.go
+++ reset.go
@@ -3,8 +3,8 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-    cfg.fileserverHits.Store(0)
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("OK"))
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
